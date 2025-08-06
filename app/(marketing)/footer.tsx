a5c84a7ffc08a92c98eba1a1666b7e40732f37f5
xxMudCloudxx <xxbaizero@gmail.com>
2025-08-07 01:14:56 +0800

    feat：完成footer组件:国家旗帜页脚

--- app/(marketing)/footer.tsx
+++ app/(marketing)/footer.tsx
@@ -1,7 +1,71 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
 export const Footer = () => {
   return (
     <footer className="hidden lg:block h-20 w-full border-t-2 border-slate-200 p-2">
-      Footer
+      <div className="max-w-screen-lg mx-auto flex items-center justify-evenly h-full">
+        <Button size={"lg"} variant={"ghost"}>
+          <Image
+            src={"/hr.svg"}
+            alt="Croatian"
+            height={32}
+            width={40}
+            className="mr-4 rounded-md"
+          />
+          Croatian
+        </Button>
+        <Button size={"lg"} variant={"ghost"}>
+          <Image
+            src={"/es.svg"}
+            alt="Spanish"
+            height={32}
+            width={40}
+            className="mr-4 rounded-md"
+          />
+          Spanish
+        </Button>
+        <Button size={"lg"} variant={"ghost"}>
+          <Image
+            src={"/fr.svg"}
+            alt="French"
+            height={32}
+            width={40}
+            className="mr-4 rounded-md"
+          />
+          French
+        </Button>
+        <Button size={"lg"} variant={"ghost"}>
+          <Image
+            src={"/it.svg"}
+            alt="Italian"
+            height={32}
+            width={40}
+            className="mr-4 rounded-md"
+          />
+          Italian
+        </Button>
+        <Button size={"lg"} variant={"ghost"}>
+          <Image
+            src={"/jp.svg"}
+            alt="Jpanese"
+            height={32}
+            width={40}
+            className="mr-4 rounded-md"
+          />
+          Jpanese
+        </Button>
+        <Button size={"lg"} variant={"ghost"}>
+          <Image
+            src={"/cn.svg"}
+            alt="Chinese"
+            height={32}
+            width={40}
+            className="mr-4 rounded-md"
+          />
+          Chinese
+        </Button>
+      </div>
     </footer>
   );
 };
