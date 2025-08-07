6e706cfa173fe4def7a6aa72dac5397953cf14dd
xxMudCloudxx <xxbaizero@gmail.com>
2025-08-08 01:19:37 +0800

    feat: 为learn页面添加用户进度组件，并使用Drizzle ORM来连接Neon数据库

---
+++ db/drizzle.ts
@@ -0,0 +1,9 @@
+import { neon } from "@neondatabase/serverless";
+import { drizzle } from "drizzle-orm/neon-http";
+
+import * as schema from "./schema";
+
+const sql = neon(process.env.DATABASE_URL!);
+const db = drizzle({ client: sql, schema: schema });
+
+export default db;
