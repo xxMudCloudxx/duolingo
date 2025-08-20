ce7758c35dd89c1cd982d1262f1d3c496188dfee
xxMudCloudxx <xxbaizero@gmail.com>
2025-08-20 15:13:36 +0800

    feat(shop): 添加商店页面和心形补充功能
    实现商店页面布局和心形补充功能，用户可以使用积分补充心形
    添加相关组件和API逻辑，包括积分扣除和心形数量更新

--- actions/user-progress.ts
+++ actions/user-progress.ts
@@ -103,3 +103,32 @@ export const reduceHearts = async (challengeId: number) => {
   revalidatePath("/leaderboard");
   revalidatePath(`/lesson/${challenge.lessonId}`);
 };
+const POINTS_TO_REFILL = 10;
+export const refillHearts = async () => {
+  const currentUserProgress = await getUserProgress();
+
+  if (!currentUserProgress) {
+    throw new Error("UserProgress not found");
+  }
+
+  if (currentUserProgress.hearts === 5) {
+    throw new Error("Hearts are already full");
+  }
+
+  if (currentUserProgress.points < POINTS_TO_REFILL) {
+    throw new Error("Not enough points");
+  }
+
+  await db
+    .update(userProgress)
+    .set({
+      hearts: 5,
+      points: currentUserProgress.points - POINTS_TO_REFILL,
+    })
+    .where(eq(userProgress.userId, currentUserProgress.userId));
+
+  revalidatePath("/learn");
+  revalidatePath("/shop");
+  revalidatePath("/quests");
+  revalidatePath("/leaderboard");
+};
