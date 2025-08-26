285b38b7a45bfd465f66f4622e42043d450afd68
xxMudCloudxx <xxbaizero@gmail.com>
2025-08-26 18:16:56 +0800

    feat: 添加下拉菜单组件

--- app/(marketing)/user-profile/[[...user-profile]]/page.tsx
+++ app/(marketing)/user-profile/[[...user-profile]]/page.tsx
@@ -1,14 +1,24 @@
+"use client";
 import { UserProfile } from "@clerk/nextjs";
+import { LogOut } from "lucide-react";
 
 const UserProfilePage = () => (
   <UserProfile
+    path="/user-profile"
+    routing="path"
     appearance={{
       elements: {
         cardBox: "shadow-none! sm:max-h-[600px]!  border-1!",
         footer: "hidden!",
       },
     }}
-  />
+  >
+    <UserProfile.Link
+      label="Back to Learn"
+      labelIcon={<LogOut size={16} />}
+      url="/learn"
+    />
+  </UserProfile>
 );
 
 export default UserProfilePage;
